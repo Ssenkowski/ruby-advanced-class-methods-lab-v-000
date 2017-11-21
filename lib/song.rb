dad54105fb0bdb488d718da9a480d9561fe63e80
Steven Senkowski <scsenkowski@gmail.com>
2017-11-21 17:38:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,7 @@ class Song
     end
 
     def self.find_or_create_by_name(name)
-      if self.find_by_name(name)
-        s
-      else self.create_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
       end
     end
   end
